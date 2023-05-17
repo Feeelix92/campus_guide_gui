27944d1561e1ee117dc49b70238235a4aa76ad56
Felix Maurer <Feeelix92@users.noreply.github.com>
2023-05-17 10:22:01 +0200

    added drawer and placeholders for services

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Campus Guide'),
@@ -85,6 +85,50 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            ListTile(
+              leading: const Icon(
+                Icons.home,
+              ),
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.calendar_month_outlined,
+              ),
+              title: const Text('Kalender'),
+              onTap: () {
+                // Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.chat,
+              ),
+              title: const Text('Nachrichten'),
+              onTap: () {
+                // Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.location_city,
+              ),
+              title: const Text('Locations'),
+              onTap: () {
+               // Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
---
+++ lib/screens/locations.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Locations extends StatefulWidget {
+  const Locations({Key? key}) : super(key: key);
+
+  @override
+  State<Locations> createState() => _LocationsState();
+}
+
+class _LocationsState extends State<Locations> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
