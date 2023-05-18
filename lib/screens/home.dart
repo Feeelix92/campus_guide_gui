1a0551d1f18d553bfa8c69265fcca39d998d0eef
Nils Friedrich <nfriedrich@pagemachine.de>
2023-05-18 18:07:13 +0200

    WIP first responsive test

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,20 +14,95 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
+
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(),
-      drawer: AppDrawer(),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            H1(
-              text: 'Herzlich Willkommen im Campus Guide!',
-            ),
-          ],
+    var displayWidth = MediaQuery.of(context).size.width;
+    if (displayWidth < 600) {
+      return const Scaffold(
+        appBar: CustomAppBar(),
+        drawer: AppDrawer(),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              H1(
+                text: 'Herzlich Willkommen im Campus Guide!',
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    } else {
+      return Scaffold(
+        appBar: CustomAppBar(),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: SizedBox(
+              width: 250.0,
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  ListTile(
+                    leading: const Icon(
+                      Icons.home,
+                    ),
+                    title: const Text('Home'),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.pushNamed(context, '/');
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.calendar_month_outlined,
+                    ),
+                    title: const Text('Kalender'),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.pushNamed(context, '/calendar');
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.chat,
+                    ),
+                    title: const Text('Nachrichten'),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.pushNamed(context, '/news');
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(
+                        Icons.location_on
+                    ),
+                    title: const Text('Locations'),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.pushNamed(context, '/locations');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: SizedBox(
+              width: displayWidth-250.0,
+              child: Column(
+                children: <Widget>[
+                  H1(
+                    text: 'Herzlich Willkommen im Campus Guide!',
+                  ),
+                ],
+              ),
+            ),
+          ),
+    ])
+      );
+    }
   }
 }
 
