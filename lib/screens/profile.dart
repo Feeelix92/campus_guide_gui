29f6f9a3af41e39612626aca397776f3ef620a48
pascalblock <mail@pascalblock.de>
2023-05-27 12:16:11 +0200

    add basic structure

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import '../widgets/appDrawer.dart';
+import '../widgets/customAppBar.dart';
+import '../widgets/h2.dart';
+
 
 class UserProfile extends StatelessWidget {
   const UserProfile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      appBar: CustomAppBar(),
+      drawer: AppDrawer(),
+      body: Center(
+        child: Column(
+          children: [
+            H2(text: 'Profile'),
+            Placeholder(),
+          ],
+        ),
+      ),
+    );
   }
 }
