a731ddd67a4ecc62207fdb7ae43ecd208b6a6683
Nils Friedrich <nfriedrich@pagemachine.de>
2023-06-09 15:23:41 +0200

    WIP Add basic marker

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,11 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
 import 'package:latlong2/latlong.dart';
 import '../widgets/appDrawer.dart';
 import '../widgets/customAppBar.dart';
 import '../widgets/h1.dart';
-import '../widgets/h2.dart';
+import '../widgets/example_popup.dart';
 
 @RoutePage()
 class LocationScreen extends StatefulWidget {
@@ -16,6 +17,37 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+  late final List<Marker> _markers;
+
+  /// Used to trigger showing/hiding of popups.
+  final PopupController _popupLayerController = PopupController();
+  @override
+  void initState() {
+    super.initState();
+    _markers = [
+      LatLng(50.33760, 8.75590),
+      LatLng(50.33740, 8.75550),
+      LatLng(50.33730, 8.75500),
+    ]
+        .map(
+          (markerPosition) => Marker(
+        point: markerPosition,
+        width: 40,
+        height: 40,
+        builder: (_) => const Icon(Icons.location_on, size: 40),
+        anchorPos: AnchorPos.align(AnchorAlign.top),
+        rotateAlignment: AnchorAlign.top.rotationAlignment,
+      ),
+    )
+        .toList();
+  }
+
+  @override
+  void dispose() {
+    _popupLayerController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,6 +69,16 @@ class _LocationScreenState extends State<LocationScreen> {
                       urlTemplate:
                       'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                       userAgentPackageName: 'dev.easy-ticket.map',
+                    ),
+                    PopupMarkerLayer(
+                      options: PopupMarkerLayerOptions(
+                        popupController: _popupLayerController,
+                        markers: _markers,
+                        popupDisplayOptions: PopupDisplayOptions(
+                          builder: (BuildContext context, Marker marker) =>
+                              ExamplePopup(marker),
+                        ),
+                      ),
                     ),
                   ]),
             )
