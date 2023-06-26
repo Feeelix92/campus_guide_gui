d9801253659b07a41911c67bf50ec2d218107840
Nils Friedrich <nfriedrich@pagemachine.de>
2023-06-26 10:07:19 +0200

    WIP Add text orientation to left

--- lib/screens/message_edit_screen.dart
+++ lib/screens/message_edit_screen.dart
@@ -70,144 +70,148 @@ class _MessageEditScreenState extends State<MessageEditScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: const CustomAppBar(),
       drawer: const AppDrawer(),
       body: SingleChildScrollView(
         child: Center(
-          child: Column(
-            children: [
-              if (finish) ...[
-                FutureBuilder(
-                    future: messageData,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData){
-                        if (snapshot.data!.owner != username) {
-                          return const Text('Sie haben keine Berechtigung diesen Beitrag zu bearbeiten.');
-                        }
-                        else if(snapshot.data!.owner == username){
-                          _titleController.text = snapshot.data!.title!;
-                          _textController.text = snapshot.data!.text!;
-                          _teaserController.text = snapshot.data!.teaser!;
-                          _authorController.text = snapshot.data!.author!;
-                          _tags = snapshot.data!.tags!;
-                          return Center(
-                            child: SizedBox(
-                              width: MediaQuery.of(context).size.width / 3,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  const H1(text: 'Bearbeiten Sie ihren Beitrag'),
-                                  const SizedBox(height: 20),
-                                  TextField(
-                                    controller: _titleController,
-                                    decoration: const InputDecoration(
-                                      labelText: 'Titel',
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  TextField(
-                                    controller: _teaserController,
-                                    keyboardType: TextInputType.multiline,
-                                    maxLines: null,
-                                    decoration: const InputDecoration(
-                                      labelText: 'Teaser',
-                                    ),
-                                  ),
-                                  TextField(
-                                    controller: _textController,
-                                    keyboardType: TextInputType.multiline,
-                                    maxLines: null,
-                                    decoration: const InputDecoration(
-                                      labelText: 'Text',
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  TextField(
-                                    controller: _authorController,
-                                    decoration: const InputDecoration(
-                                      labelText: 'Autor',
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  TextField(
-                                    controller: _tagController,
-                                    inputFormatters: <TextInputFormatter>[
-                                      FilteringTextInputFormatter.allow(RegExp("[0-9a-zA-Z]")),
-                                    ], // Only
-                                    decoration: InputDecoration(
-                                      labelText: 'Tag hinzufügen',
-                                      hintText: '...',
-                                      suffixIcon: IconButton(
-                                        icon: const Icon(Icons.add),
-                                        onPressed: () {
-                                          _addTag(_tagController.text);
-                                          _tagController.clear();
-                                        },
+          child: Container(
+            child: Column(
+              children: [
+                if (finish) ...[
+                  FutureBuilder(
+                      future: messageData,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData){
+                          if (snapshot.data!.owner != username) {
+                            return const Text('Sie haben keine Berechtigung diesen Beitrag zu bearbeiten.');
+                          }
+                          else if(snapshot.data!.owner == username){
+                            _titleController.text = snapshot.data!.title!;
+                            _textController.text = snapshot.data!.text!;
+                            _teaserController.text = snapshot.data!.teaser!;
+                            _authorController.text = snapshot.data!.author!;
+                            _tags = snapshot.data!.tags!;
+                            return Center(
+                              child: SizedBox(
+                                width: screenWidth-0.1*screenWidth,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const H1(text: 'Bearbeiten Sie ihren Beitrag'),
+                                    const SizedBox(height: 20),
+                                    TextField(
+                                      controller: _titleController,
+                                      decoration: const InputDecoration(
+                                        labelText: 'Titel',
                                       ),
                                     ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Wrap(
-                                    spacing: 8,
-                                    children: _tags.map((tag) {
-                                      return Chip(
-                                        label: Text(tag),
-                                        onDeleted: () => _removeTag(tag),
-                                      );
-                                    }).toList(),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      String titel = _titleController.text;
-                                      String text = _textController.text;
-                                      String teaser = _teaserController.text;
-                                      String author = _authorController.text;
-
-                                      // Beispiel-Validierung: Überprüfen, ob alle Felder nicht leer sind
-                                      if (titel.isNotEmpty &&
-                                          text.isNotEmpty &&
-                                          author.isNotEmpty) {
-                                        message.putMessageData(
-                                            snapshot.data!.id! ,titel, text, teaser, author, _tags);
-                                        AutoRouter.of(context).push(const MessageRoute());
-                                      } else {
-                                        showDialog(
-                                          context: context,
-                                          builder: (context) => AlertDialog(
-                                            title: const Text('Fehler'),
-                                            content: const Text(
-                                                'Bitte füllen Sie alle Felder aus.'),
-                                            actions: [
-                                              TextButton(
-                                                onPressed: () {
-                                                  AutoRouter.of(context).pop();
-                                                },
-                                                child: const Text('OK'),
-                                              ),
-                                            ],
-                                          ),
+                                    const SizedBox(height: 10),
+                                    TextField(
+                                      controller: _teaserController,
+                                      keyboardType: TextInputType.multiline,
+                                      maxLines: null,
+                                      decoration: const InputDecoration(
+                                        labelText: 'Teaser',
+                                      ),
+                                    ),
+                                    TextField(
+                                      controller: _textController,
+                                      keyboardType: TextInputType.multiline,
+                                      maxLines: null,
+                                      decoration: const InputDecoration(
+                                        labelText: 'Text',
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    TextField(
+                                      controller: _authorController,
+                                      decoration: const InputDecoration(
+                                        labelText: 'Autor',
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    TextField(
+                                      controller: _tagController,
+                                      inputFormatters: <TextInputFormatter>[
+                                        FilteringTextInputFormatter.allow(RegExp("[0-9a-zA-Z]")),
+                                      ], // Only
+                                      decoration: InputDecoration(
+                                        labelText: 'Tag hinzufügen',
+                                        hintText: '...',
+                                        suffixIcon: IconButton(
+                                          icon: const Icon(Icons.add),
+                                          onPressed: () {
+                                            _addTag(_tagController.text);
+                                            _tagController.clear();
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Wrap(
+                                      spacing: 8,
+                                      children: _tags.map((tag) {
+                                        return Chip(
+                                          label: Text(tag),
+                                          onDeleted: () => _removeTag(tag),
                                         );
-                                      }
-                                    },
-                                    child: const Text('Nachricht posten'),
-                                  ),
-                                  const SizedBox(height: 10),
-                                ],
+                                      }).toList(),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        String titel = _titleController.text;
+                                        String text = _textController.text;
+                                        String teaser = _teaserController.text;
+                                        String author = _authorController.text;
+
+                                        // Beispiel-Validierung: Überprüfen, ob alle Felder nicht leer sind
+                                        if (titel.isNotEmpty &&
+                                            text.isNotEmpty &&
+                                            author.isNotEmpty) {
+                                          message.putMessageData(
+                                              snapshot.data!.id! ,titel, text, teaser, author, _tags);
+                                          AutoRouter.of(context).push(const MessageRoute());
+                                        } else {
+                                          showDialog(
+                                            context: context,
+                                            builder: (context) => AlertDialog(
+                                              title: const Text('Fehler'),
+                                              content: const Text(
+                                                  'Bitte füllen Sie alle Felder aus.'),
+                                              actions: [
+                                                TextButton(
+                                                  onPressed: () {
+                                                    AutoRouter.of(context).pop();
+                                                  },
+                                                  child: const Text('OK'),
+                                                ),
+                                              ],
+                                            ),
+                                          );
+                                        }
+                                      },
+                                      child: const Text('Nachricht posten'),
+                                    ),
+                                    const SizedBox(height: 10),
+                                  ],
+                                ),
                               ),
-                            ),
-                          );
+                            );
+                          }
+                        } else if (snapshot.hasError) {
+                          return Text('${snapshot.error}');
                         }
-                      } else if (snapshot.hasError) {
-                        return Text('${snapshot.error}');
-                      }
-                      return const CircularProgressIndicator();
-                    }),
-              ] else ...[
-                const CircularProgressIndicator()
+                        return const CircularProgressIndicator();
+                      }),
+                ] else ...[
+                  const CircularProgressIndicator()
+                ],
               ],
-            ],
+            ),
           ),
         ),
       ),
